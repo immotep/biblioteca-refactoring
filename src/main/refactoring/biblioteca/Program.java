d9ddd13e26c1fbc556e4d9e991a25dc17dca436e
NavaL Andrian <immotep@gmail.com>
2012-09-30 23:37:21 +0200

    extracted Program.logUserIn()

--- src/main/refactoring/biblioteca/Program.java
+++ src/main/refactoring/biblioteca/Program.java
@@ -27,25 +27,7 @@ public class Program {
             } else if (i1 == 4) {
                 printMovieCatalog();
             } else if (i1 == 5) {
-                clearLogin();
-                System.out.println("Enter your library number");
-                try {
-                    String libraryNumber = reader.readLine();
-                    if (validLibraryNumber(libraryNumber)) {
-                        try {
-                            System.out.println("Enter your Password: ");
-                            String password = reader.readLine();
-                            if (validPassword(password)) {
-                                loggedIn = true;
-                                savedLibraryNumber = libraryNumber;
-                            }
-                        } catch (Exception e) {
-
-                        }
-                    }
-                } catch (Exception e) {
-
-                }
+                logUserIn(reader);
 
             } else if (i1 == 9) {
                 System.out.println("Quitting...");
@@ -54,6 +36,28 @@ public class Program {
                 System.out.println("\n");
                 System.out.println("Enter a valid integer!!");
             }
+        }
+    }
+
+    private static void logUserIn(BufferedReader reader) {
+        clearLogin();
+        System.out.println("Enter your library number");
+        try {
+            String libraryNumber = reader.readLine();
+            if (validLibraryNumber(libraryNumber)) {
+                try {
+                    System.out.println("Enter your Password: ");
+                    String password = reader.readLine();
+                    if (validPassword(password)) {
+                        loggedIn = true;
+                        savedLibraryNumber = libraryNumber;
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        } catch (Exception e) {
+
         }
     }
 
