d6d9eaec95b89ad87cef9a10d07d25f7f53f424c
NavaL Andrian <immotep@gmail.com>
2012-09-30 21:55:36 +0200

    extracted printWelcome and printMenu() in Program.java

--- src/main/refactoring/biblioteca/Program.java
+++ src/main/refactoring/biblioteca/Program.java
@@ -9,19 +9,8 @@ public class Program {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("**********************************************************");
-            System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
-            System.out.println("**********************************************************");
-            System.out.println("*                Menu                                    *");
-            System.out.println("*         =====================                          *");
-            System.out.println("*         1. List Book Catalog                           *");
-            System.out.println("*         2. Check out Book                              *");
-            System.out.println("*         3. Check Library Number                        *");
-            System.out.println("*         4. Movie Listing                               *");
-            System.out.println("*         5. Login                                       *");
-            System.out.println("*         9. Exit                                        *");
-            System.out.println("**********************************************************");
-            System.out.println("Your Selection: ");
+            printWelcome();
+            printMenu();
 
             InputStreamReader inputStream = new InputStreamReader(System.in);
             BufferedReader reader = new BufferedReader(inputStream);
@@ -120,6 +109,25 @@ public class Program {
                 System.out.println("Enter a valid integer!!");
             }
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+        System.out.println("Your Selection: ");
+    }
+
+    private static void printWelcome() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
     }
 
     private static boolean validPassword(String password) {
