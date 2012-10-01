607a1ac129dd9e5b88a50d69ab53c2264b6fc527
NavaL Andrian <immotep@gmail.com>
2012-10-01 07:25:53 +0200

    removed Program.printAllBooks()

--- src/main/refactoring/biblioteca/Program.java
+++ src/main/refactoring/biblioteca/Program.java
@@ -113,13 +113,6 @@ public class Program {
         }
     }
 
-    protected void printAllBooks() {
-        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
-        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
-        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
-        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
-    }
-
     public  int getUserInput(BufferedReader reader) {
         int i1 = 0;
         try {
