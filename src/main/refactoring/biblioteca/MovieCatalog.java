9b7842c2fbdf53723009c6899bcd85700420231f
NavaL Andrian <immotep@gmail.com>
2012-10-01 07:48:54 +0200

    added createAllMovies() and listAllMovies() in MovieCatalog

--- src/main/refactoring/biblioteca/MovieCatalog.java
+++ src/main/refactoring/biblioteca/MovieCatalog.java
@@ -1,8 +1,41 @@
 package refactoring.biblioteca;
 
-public class MovieCatalog {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MovieCatalog {
+    
+    private List<String> allMovies = new ArrayList<String>();
+
+    public MovieCatalog() {
+        createAllMovies();
+    }
+
+    private void createAllMovies(){
+        allMovies.add(createMovie("Rocky", "John G. Avildsen", "10"));
+        allMovies.add(createMovie("Rocky II", "John G. Avildsen", "9"));
+        allMovies.add(createMovie("Rocky III", "John G. Avildsen", "8"));
+        allMovies.add(createMovie("Rocky IV", "John G. Avildsen", "7"));
+        allMovies.add(createMovie("Rocky V", "John G. Avildsen", "8"));
+        allMovies.add(createMovie("Drainage", "Francisco Trindade", "N/A"));
+        allMovies.add(createMovie("The Shawshank Redemption", "Frank Darabont", "10"));
+        allMovies.add(createMovie("The Godfather", "Francis Ford Coppola", "7"));
+        allMovies.add(createMovie("Inception", "Frank Darabont", "10"));
+        allMovies.add(createMovie("Pulp Fiction", "Quentin Tarantino", "6"));
+        
+    }
+    
     protected  String createMovie(String movieTitle, String movieDirector, String movieRanking) {
         return movieTitle + " - Director: " + movieDirector + " Rating: " + movieRanking;
     }
+
+    protected String listOfMovies(){
+        StringBuilder list = new StringBuilder();
+        for (String s : allMovies){
+            list.append(s).append("\n");
+        }
+
+        return list.toString();
+    }
+
 }
--- src/test/refactoring/biblioteca/MovieCatalogTest.java
+++ src/test/refactoring/biblioteca/MovieCatalogTest.java
@@ -18,4 +18,22 @@ public class MovieCatalogTest {
 
         assertEquals(expected, movies.createMovie(movieTitle, movieDirector, movieRanking));
     }
+
+    @Test
+    public void testListOfAllMoviesIsAppendedWithNewLineForEachMovie() throws Exception {
+        MovieCatalog movies = new MovieCatalog();
+
+        String expected = "Rocky - Director: John G. Avildsen Rating: 10" + "\n" +
+        "Rocky II - Director: John G. Avildsen Rating: 9" + "\n" +
+       "Rocky III - Director: John G. Avildsen Rating: 8" + "\n" +
+        "Rocky IV - Director: John G. Avildsen Rating: 7"  + "\n" +
+        "Rocky V - Director: John G. Avildsen Rating: 8"   + "\n" +
+        "Drainage - Director: Francisco Trindade Rating: N/A"      + "\n" +
+        "The Shawshank Redemption - Director: Frank Darabont Rating: 10"   + "\n" +
+        "The Godfather - Director: Francis Ford Coppola Rating: 7"  + "\n" +
+        "Inception - Director: Frank Darabont Rating: 10"  + "\n" +
+        "Pulp Fiction - Director: Quentin Tarantino Rating: 6";
+
+        assertEquals(expected, movies.listOfMovies().trim());
+    }
 }
